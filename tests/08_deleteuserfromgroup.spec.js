a2ce6226c2ae73df0e3b6f6e37a89853a7dd2997
Tehreem Fatima <expk0328@pakoxygen.com>
2026-03-09 09:23:44 +0500

    fixed

--- tests/08_deleteuserfromgroup.spec.js
+++ tests/08_deleteuserfromgroup.spec.js
@@ -7,7 +7,7 @@ test('Edit Group - Delete User From The Group', async({page}) =>
 
         const polusername = "Tehreem.Fatima@pakoxygen.com";
         const polpassword = "P@kistan01";
-        const automation = "automation 9";
+        const automation = "automation 10";
         const automationgroup = "Test Automation Group 3";
         const polautomation = "automation"
         
